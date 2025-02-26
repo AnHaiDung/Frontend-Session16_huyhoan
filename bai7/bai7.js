83ca1fb9d6add79a70ced115e17dde4a2e8e49f2
AnHaiDung <anhaidung2k6@gmail.com>
2025-02-26 17:03:33 +0700

    Add files via upload

---
+++ bai7/bai7.js
@@ -0,0 +1,10 @@
+
+let math = prompt("Nhập vào điểm toán : ");
+
+let physics = prompt("Nhập vào điểm vật lí : ");
+
+let chemistry = prompt("Nhập vào điểm hóa học : ");
+
+let result = (+math + +physics + +chemistry) / +3;
+
+document.write("Điểm trung bình 3 môn là : " + result);
