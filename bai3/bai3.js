a5f426e86bcc44adf1c37873dce648d86003c0be
AnHaiDung <anhaidung2k6@gmail.com>
2025-02-26 17:01:24 +0700

    Add files via upload

---
+++ bai3/bai3.js
@@ -0,0 +1,8 @@
+
+console.log("wellcome to Rikkei Academy");
+
+document.write("Quyết tâm học lập trình javascript");
+
+alert("Chào mừng bạn đến RIKKEI");
+
+prompt("Bạn đã đủ 18 tuổi chưa ? (Yes or No)");
