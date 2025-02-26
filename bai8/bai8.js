37c256a19dfbde56bdf20836891f926d1d2e72c3
AnHaiDung <anhaidung2k6@gmail.com>
2025-02-26 17:03:51 +0700

    Add files via upload

---
+++ bai8/bai8.js
@@ -0,0 +1,10 @@
+
+let a = prompt("Nhập vào số đầu tiên : ");
+
+let b = prompt("Nhập vào số thứ hai : ");
+
+let c = prompt("Nhập vào số thứ ba  : ");
+
+let result = Math.max(+a,+b,+c);
+
+document.write("Số lớn nhất là : " + result);
